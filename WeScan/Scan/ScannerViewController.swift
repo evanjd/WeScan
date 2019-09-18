e03ad6ffd9f1e42ffd324972cc866081c0a6e75f
Evan Bruhn <evan.bruhn@gmail.com>
2019-09-19 09:50:15 +1000

    Do not add blurred background to camera nav bar on iOS 13

--- WeScan/Scan/ScannerViewController.swift
+++ WeScan/Scan/ScannerViewController.swift
@@ -98,10 +98,13 @@ final class ScannerViewController: UIViewController {
 
         navigationController?.navigationBar.isTranslucent = true
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
-        navigationController?.navigationBar.addSubview(visualEffectView)
-        navigationController?.navigationBar.sendSubviewToBack(visualEffectView)
-        
-        navigationController?.navigationBar.barStyle = .blackTranslucent
+
+        if #available(iOS 13.0, *) {} else {
+            navigationController?.navigationBar.addSubview(visualEffectView)
+            navigationController?.navigationBar.sendSubviewToBack(visualEffectView)
+            navigationController?.navigationBar.barStyle = .blackTranslucent
+        }
+
     }
     
     override func viewDidLayoutSubviews() {
